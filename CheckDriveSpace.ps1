1f76bdcdd13c2c5914e644feb0fd2d9f8df61eb4
Jackson Luo <136623968+jluo1875@users.noreply.github.com>
2024-01-22 02:30:08 -0600

    Create CheckDriveSpace.ps1

---
+++ CheckDriveSpace.ps1
@@ -0,0 +1,26 @@
+﻿<#
+This PowerShell script checks a drive for free space left, with a minimum of 5 GB. 
+
+Usage:
+PS> ./check-drive-space C
+#>
+
+param([string]$Drive = "", [int]$MinLevel = 5)
+
+try {
+	
+	if ($Free -lt $MinLevel) {
+        	write-warning "Drive $Drive has only $Free GB left to use! ($Used of $Total GB used, minimum is $MinLevel GB)"
+		exit 1
+  
+ if ($Drive -eq "" ) { $Drive = read-host "Enter drive to check" }
+
+	$DriveDetails = (get-psdrive $Drive)
+	[int]$Free = (($DriveDetails.Free / 512) / 512) / 512
+	[int]$Used = (($DriveDetails.Used / 512) / 512) / 512
+	[int]$Total = ($Used + $Free)
+	}
+
+	& "$PSScriptRoot/speak-english.ps1" "Drive $Drive has $Free GB left ($Total GB total)"
+	exit 0
+}
