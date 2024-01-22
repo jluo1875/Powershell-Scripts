18d3a9befe313c6cc9d826f49ae0fc8af03775fa
Jackson Luo <136623968+jluo1875@users.noreply.github.com>
2024-01-22 01:30:48 -0600

    Edited script

--- BlockNetworkTraffic.ps1
+++ BlockNetworkTraffic.ps1
@@ -1,21 +1,21 @@
 function BlockNetworkTraffic {
 
-     <# 
-      DESCRIPTION
-      Blocks network traffic destined to the provided IP address and/or port, adds a rule to the local Windows Firewall policy so network traffic destined to the provided IP address and/or port is blocked.
+<# 
+DESCRIPTION
+Blocks network traffic destined to the provided IP address and/or port, adds a rule to the local Windows Firewall policy so network traffic destined to the provided IP address and/or port is blocked.
 
-      PARAMETER Protocol
-      Specifies the protocol to block.
+PARAMETER Protocol
+Specifies the protocol to block.
 
-      PARAMETER IPAddress
-      Specifies the IP address to block traffic.
+PARAMETER IPAddress
+Specifies the IP address to block traffic.
 
-      PARAMETER Port
-      Specifies the port to block traffic.
+PARAMETER Port
+Specifies the port to block traffic.
 
-      EXAMPLE
-      PS> BlockNetworkTraffic -Protocol UDP -IpAddress 8.8.8.8 -Port 53
-     #>
+EXAMPLE
+PS> BlockNetworkTraffic -Protocol UDP -IpAddress 8.8.8.8 -Port 53
+#>
   Param(
       [ValidateSet("Any","TCP","UDP","ICMPv4","ICMPv6")][string]$Protocol = "Any",
       [Parameter(Mandatory)][ipaddress]$IpAddress,
