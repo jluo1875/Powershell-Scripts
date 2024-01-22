eca5b1599d0689002132ad2d17626d08a29c42ee
Jackson Luo <136623968+jluo1875@users.noreply.github.com>
2024-01-22 03:01:18 -0600

    Included link

--- DisableFirewall.ps1
+++ DisableFirewall.ps1
@@ -4,6 +4,9 @@ Disables the domain, public, and private firewall profile.
 
 Usage:
 PS> DisableFirewall
+
+Link: 
+github.com/jluo1875/Powershell-Scripts
 #>
   Set-NetFirewallProfile -Name domain,public,private -Enabled False
 } 
