b6d8bc2c554dcd4b860442a2c3dd1f2b7b2bf23c
Jackson Luo <136623968+jluo1875@users.noreply.github.com>
2024-01-22 03:06:23 -0600

    Cleaned up code

--- OpenPowershellModule.ps1
+++ OpenPowershellModule.ps1
@@ -1,4 +1,5 @@
 function OpenPowershellModule {
+
 <#
 Opens the specified PowerShell script module file (.psm1) using PowerShell ISE. 
 
@@ -8,6 +9,7 @@ PS> Edit-Module "soap"
 Link:
 github.com/jluo1875/Powershell-Scripts
 #>
+
   Param([Parameter(Mandatory)][string]$Name)
   $Module = Get-Module | Where-Object { $_.Path -like "*$Name.psm1" }
   if ($Module) { 
